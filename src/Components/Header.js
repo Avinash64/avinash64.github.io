f1d2edd8fcc0c2287496916525dcae47545f5daf
hop <avinashseelall@gmail.com>
2023-04-02 15:31:45 -0400

    added project component

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        
+    <div className='name'>Avinash Seelall</div>
+    <div className='contact'>
+            <a href='https://www.linkedin.com/in/avinash-seelall-b42b64167'>linkedin</a>
+            <a href='https://github.com/Avinash64/'>github</a>
+    </div>
+    </div>
+  )
+}
+
+export default Header
